149781959222a20609807d5bbdcd977a01bbfe04
Luiz Gustavo da Silva <38591825+Luiz4us8av6@users.noreply.github.com>
2024-12-30 20:00:17 -0300

    feat: criação de validações para cadastro de produto

--- src/app/shared/product-modal/product-modal.component.ts
+++ src/app/shared/product-modal/product-modal.component.ts
@@ -1,10 +1,20 @@
 import { CommonModule } from '@angular/common';
-import { Component, Inject } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { Component, Inject, OnInit } from '@angular/core';
+import {
+  FormBuilder,
+  FormGroup,
+  FormsModule,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
-import { MAT_DIALOG_DATA, MatDialogModule, MatDialogRef } from '@angular/material/dialog';
+import {
+  MAT_DIALOG_DATA,
+  MatDialogModule,
+  MatDialogRef,
+} from '@angular/material/dialog';
 import { Product } from '../../core/models/product-interface';
 import { ProductsService } from '../../core/services/products.service';
 import { MatSnackBar } from '@angular/material/snack-bar';
@@ -20,11 +30,13 @@ import { SnackbarService } from '../../core/services/snackbar.service';
     MatButtonModule,
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
   ],
   templateUrl: './product-modal.component.html',
-  styleUrl: './product-modal.component.scss'
+  styleUrl: './product-modal.component.scss',
 })
-export class ProductModalComponent {
+export class ProductModalComponent implements OnInit {
+  form!: FormGroup;
   product: Product = {
     name: '',
     price: 0,
@@ -38,12 +50,22 @@ export class ProductModalComponent {
     public dialogRef: MatDialogRef<ProductModalComponent>,
     @Inject(MAT_DIALOG_DATA) public data: any,
     private productsService: ProductsService,
-    private snackbarService: SnackbarService
+    private snackbarService: SnackbarService,
+    private fb: FormBuilder
   ) {
     if (data) {
       this.action = true;
-      this.product ={...data}
+      this.product = { ...data };
     }
+  }
+
+  ngOnInit(): void {
+    this.form = this.fb.nonNullable.group({
+      name: [this.product.name, [Validators.required]],
+      price: [this.product.price, [Validators.required, Validators.min(0)]],
+      category: [this.product.category, Validators.required],
+      description: [this.product.description, Validators.required],
+    });
   }
 
   onClose(): void {
@@ -51,31 +73,61 @@ export class ProductModalComponent {
   }
 
   onSave(): void {
-    this.action ? this.updateProduct() : this.addProduct();
+    if (this.form.invalid) {
+      this.snackbarService.showSnackBar(
+        'Por favor, corrija os erros antes de salvar.',
+        'error'
+      );
+      return;
+    }
+
+    const formValues = this.form.getRawValue();
+
+    const product: Product = {
+      name: formValues.name,
+      price: formValues.price,
+      category: formValues.category,
+      description: formValues.description,
+    };
+
+    this.action ? this.updateProduct(product) : this.addProduct(product);
     this.dialogRef.close(this.product);
   }
 
-  updateProduct() {
-    this.productsService.updateProduct(this.product).subscribe({
+  updateProduct(product: Product) {
+    product.id = Number(this.product.id);
+    this.productsService.updateProduct(product).subscribe({
       next: (response) => {
-        this.snackbarService.showSnackBar('Produto atualizado com sucesso!', 'success');
+        this.snackbarService.showSnackBar(
+          'Produto atualizado com sucesso!',
+          'success'
+        );
         this.onClose();
       },
       error: (err) => {
-        this.snackbarService.showSnackBar('Erro ao atualizar o produto.', 'error');
+        this.snackbarService.showSnackBar(
+          'Erro ao atualizar o produto.',
+          'error'
+        );
       },
     });
   }
 
-  addProduct() {
-    this.productsService.addProduct(this.product).subscribe({
+  addProduct(product: Product) {
+    this.productsService.addProduct(product).subscribe({
       next: (response) => {
-        this.snackbarService.showSnackBar('Produto adicionado com sucesso!', 'success');
+        this.snackbarService.showSnackBar(
+          'Produto adicionado com sucesso!',
+          'success'
+        );
         this.onClose();
       },
       error: (err) => {
-        this.snackbarService.showSnackBar('Erro ao produto a categoria.', 'error');
+        this.snackbarService.showSnackBar(
+          'Erro ao cadastrar o produto.',
+          'error'
+        );
       },
     });
-  } 
+  }
 }
